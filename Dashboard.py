f027076a63c26136abe76acbfdd6fa365e5a9dbe
Pratika1012 <118338761+Pratika1012@users.noreply.github.com>
2025-08-05 12:55:23 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 # import dash
 # import plotly.figure_factory as ff
 # import matplotlib.pyplot as plt
